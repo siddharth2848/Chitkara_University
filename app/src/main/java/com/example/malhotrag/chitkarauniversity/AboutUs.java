cda53e3db453a1e9efc2625977a9f8449bb07feb
siddharth2848 <siddharth2848@yahoo.in>
2016-05-21 14:46:26 +0530

    Done

--- app/src/main/java/com/example/malhotrag/chitkarauniversity/AboutUs.java
+++ app/src/main/java/com/example/malhotrag/chitkarauniversity/AboutUs.java
@@ -1,12 +1,12 @@
 package com.example.malhotrag.chitkarauniversity;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by Malhotra G on 5/20/2016.
  */
-public class AboutUs extends Activity {
+public class AboutUs extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/malhotrag/chitkarauniversity/ContactUs.java
+++ app/src/main/java/com/example/malhotrag/chitkarauniversity/ContactUs.java
@@ -1,20 +1,22 @@
 package com.example.malhotrag.chitkarauniversity;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.webkit.WebView;
+import android.widget.ImageView;
 
 /**
  * Created by Malhotra G on 5/20/2016.
  */
 public class ContactUs extends AppCompatActivity{
-    WebView webView;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.contactus_layout);
+        //imageView = (ImageView) findViewById(R.id.imageView2);
+
+        //imageView.setImageResource(R.drawable.contact_us);
 
     }
 }
